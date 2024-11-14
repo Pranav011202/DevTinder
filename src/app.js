c666133e923cfafb3aa8911a18dc4e1c5e8f93f0
Pranav Saluja <pranavsaluja12345@gmail.com>
2024-11-14 18:59:14 +0530

    Handling route handler

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("hello from the server")
+//middleware 
+app.use("/user",(req,res,next)=>{
+    //Route Handler
+    res.send("Route Handler 1");
+    next();
+},
+(req,res)=>{
+    res.send("Route Handler 2")
 });
 
-app.listen(3000 , ()=>{
-    console.log("Server is listening on PORT 3000");
+app.listen(7777 , ()=>{
+    console.log("Server is listening on PORT 7777");
 });
