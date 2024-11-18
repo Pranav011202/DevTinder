d8ea2e5a7737517b1f2d255474f4371ae3032cd3
Pranav Saluja <pranavsaluja12345@gmail.com>
2024-11-18 15:51:26 +0530

    Middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,7 @@
+const adminAuth = (req,res,next) => {
+    console.log("Admin Auth is getting checked");
+}
+
+module.exports = {
+    adminAuth,
+}
--- src/app.js
+++ src/app.js
@@ -2,15 +2,9 @@ const express = require("express");
 
 const app = express();
 
+const {adminAuth} = require("./middlewares/auth");
+
 //middleware 
-app.use("/user",(req,res,next)=>{
-    //Route Handler
-    res.send("Route Handler 1");
-    next();
-},
-(req,res)=>{
-    res.send("Route Handler 2")
-});
 
 app.listen(7777 , ()=>{
     console.log("Server is listening on PORT 7777");
